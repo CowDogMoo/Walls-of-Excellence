00084a360f8c08afbcbfe88823b20eccd1524d5f
Jayson Grace <jayson.e.grace@gmail.com>
2023-05-13 23:04:34 -0600

    DRY ansible functions up

--- magefiles/ansible.go
+++ magefiles/ansible.go
@@ -8,15 +8,6 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
-// runAnsibleCommand is a helper function that runs the given ansible command with the provided arguments.
-func runAnsibleCommand(cmd string, args ...string) error {
-	if err := sh.RunV(cmd, args...); err != nil {
-		fmt.Println(color.RedString("failed to run ansible command: %v", err))
-		return err
-	}
-	return nil
-}
-
 // AnsiblePing runs ansible all -m ping against all k8s nodes.
 //
 // Returns:
@@ -43,12 +34,40 @@ func AnsiblePing() error {
 	return runAnsibleCommand("ansible", args...)
 }
 
-// RunAnsible executes the ansible-playbook command to provision the k8s nodes on the master or node or both,
-// based on the provided group. If no group is specified, the command is executed on all groups.
+// RunReset executes the 'ansible-playbook' command with the 'reset.yml' playbook on the master or node or all,
+// based on the provided group.
 //
 // Parameters:
 //
-// group: An optional string representing the group (either "master" or "node") to provision the k8s nodes on.
+// group: A string representing the group (either "master", "node", or "all") to execute the command on.
+//
+// Returns:
+//
+// error: An error if the shell command fails.
+//
+// Example:
+//
+//	if err := RunReset("master"); err != nil {
+//	  log.Fatalf("Failed to reset master: %v", err)
+//	}
+//
+//	if err := RunReset("node"); err != nil {
+//	  log.Fatalf("Failed to reset node: %v", err)
+//	}
+//
+//	if err := RunReset("all"); err != nil {
+//	  log.Fatalf("Failed to reset: %v", err)
+//	}
+func RunReset(group string) error {
+	return runAnsiblePlaybook("reset.yml", group)
+}
+
+// RunAnsible executes the ansible-playbook command to provision the k8s nodes on the master or node or all,
+// based on the provided group.
+//
+// Parameters:
+//
+// group: A string representing the group (either "master", "node", or "all") to provision the k8s nodes on.
 //
 // Returns:
 //
@@ -64,57 +83,42 @@ func AnsiblePing() error {
 //	  log.Fatalf("Failed to run ansible playbook on node: %v", err)
 //	}
 //
-//	if err := RunAnsible(); err != nil {
+//	if err := RunAnsible("all"); err != nil {
 //	  log.Fatalf("Failed to run ansible playbook: %v", err)
 //	}
 func RunAnsible(group string) error {
-	args := []string{
-		filepath.Join("k3s-ansible", "site.yml"),
-		"-i",
-		filepath.Join("k3s-ansible", "inventory", "cowdogmoo", "hosts.ini"),
-	}
-	if len(group) > 0 {
-		args = append(args, "--limit", group)
-	} else {
-		args = append(args, "--limit", "all")
-	}
-	return runAnsibleCommand("ansible-playbook", args...)
+	return runAnsiblePlaybook("site.yml", group)
 }
 
-// RunReset executes the 'ansible-playbook' command with the 'reset.yml' playbook on the master or node or both,
-// based on the provided group. If no group is specified, the command is executed on the node group.
+// runAnsiblePlaybook runs a specific ansible playbook on a given group of nodes.
 //
 // Parameters:
 //
-// group: An optional string representing the group (either "master" or "node") to execute the command on.
+// playbook: A string representing the playbook (either "reset.yml" or "site.yml") to execute.
+//
+// group: A string representing the group (either "master", "node", or "all") to execute the command on.
 //
 // Returns:
 //
 // error: An error if the shell command fails.
-//
-// Example:
-//
-//	if err := RunReset("master"); err != nil {
-//	  log.Fatalf("failed to reset master nodes: %v", err)
-//	}
-//
-//	if err := RunReset("node"); err != nil {
-//	  log.Fatalf("failed to reset nodes: %v", err)
-//	}
-//
-//	if err := RunReset(); err != nil {
-//	  log.Fatalf("failed to reset all managed nodes: %v", err)
-//	}
-func RunReset(group string) error {
+func runAnsiblePlaybook(playbook, group string) error {
+	if group != "master" && group != "node" && group != "all" {
+		return fmt.Errorf("invalid group: %s. group must be 'master', 'node', or 'all'", group)
+	}
 	args := []string{
-		filepath.Join("k3s-ansible", "reset.yml"),
+		filepath.Join("k3s-ansible", playbook),
 		"-i",
 		filepath.Join("k3s-ansible", "inventory", "cowdogmoo", "hosts.ini"),
-	}
-	if len(group) > 0 {
-		args = append(args, "--limit", group)
-	} else {
-		args = append(args, "--limit", "node")
+		"--limit", group,
 	}
 	return runAnsibleCommand("ansible-playbook", args...)
+}
+
+// runAnsibleCommand is a helper function that runs the given ansible command with the provided arguments.
+func runAnsibleCommand(cmd string, args ...string) error {
+	if err := sh.RunV(cmd, args...); err != nil {
+		fmt.Println(color.RedString("failed to run ansible command: %v", err))
+		return err
+	}
+	return nil
 }
